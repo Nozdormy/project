b195a47ab6d67a18be06fa77af5dabed34355d2e
Sergey Goncharov <goncharov96snz@gmail.com>
2024-05-22 20:24:52 +0500

    Added filtering cycle with Stream

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -9,6 +9,7 @@ import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -25,7 +26,7 @@ public class UserMealsUtil {
         List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsTo.forEach(System.out::println);
 
-//        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
@@ -46,7 +47,19 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        List<UserMeal> filteredMeals = meals.stream()
+                .filter(meal -> meal.getDateTime().toLocalTime().isAfter(startTime) || meal.getDateTime().toLocalTime().equals(startTime))
+                .filter(meal -> meal.getDateTime().toLocalTime().isBefore(endTime) || meal.getDateTime().toLocalTime().equals(endTime))
+                .collect(Collectors.toList());
 
-        return null;
+        int totalCalories = filteredMeals.stream()
+                .mapToInt(UserMeal::getCalories)
+                .reduce(0, Integer::sum);
+
+        boolean hasExcess = totalCalories > caloriesPerDay;
+
+        return filteredMeals.stream()
+                .map(meal -> new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), hasExcess))
+                .collect(Collectors.toList());
     }
 }
